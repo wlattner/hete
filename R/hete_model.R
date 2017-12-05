cb074f3bb43d5bd18adcfe4842f8fcaa1891f7db
Bill Lattner <wlattner@gmail.com>
2017-12-04 22:42:01 -0800

    ENH: plot for hete_model (#24)

--- R/hete_model.R
+++ R/hete_model.R
@@ -1,13 +1,30 @@
 hete_model <- function(x, y, tmt, model_terms, ..., subclass) {
   attrs <- rlang::dots_list(...)
   attrs[['train_data']] <- list(x = x, y = y, tmt = tmt)
-  structure(attrs, class = c(subclass, "hete_model"))
+  m <- structure(attrs, class = c(subclass, "hete_model"))
+  add_predictions(m)
+}
+
+add_predictions <- function(obj) {
+  p <- stats::predict(obj, obj$train_data$x)
+  obj$pred_te <- p
+  obj
 }
 
 #' @export
 print.hete_model <- function(x, ...) {
   cat("A hete_model\n")
-  cat("Method: ", meta_learner_name(x), "\n")
+  cat("Method:", meta_learner_name(x), "\n")
+  cat("Predicted ATE:", mean(x$pred_te), "\n")
+}
+
+#' @export
+plot.hete_model <- function(x, y, what = c("uplift", "quantile"), ...) {
+  what <- match.arg(what)
+  switch(what,
+    uplift = plot(uplift(x$train_data$y, x$train_data$tmt, x$pred_te)),
+    quantile = plot_quantiles(x$train_data$y, x$train_data$tmt, x$pred_te)
+  )
 }
 
 meta_learner_name <- function(x) {
